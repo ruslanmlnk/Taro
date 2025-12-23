9d9481ec1b80c1a4eba12aba81e94c787aebd354
ruslanmlnk <kanevgoga2@gmail.com>
2025-12-23 11:17:51 +0200

    Set host and port for Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
+  host: '0.0.0.0', // Or simply host: true
+  port: 4321,
   output: 'server',
   adapter: node({
     mode: 'standalone'
